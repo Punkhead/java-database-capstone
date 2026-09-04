193456d292f6bfd486e80e622bbf02541fe27d0a
Punkhead <80025967+Punkhead@users.noreply.github.com>
2026-09-04 14:45:01 +0300

    Add custom message to NotNull annotation for username

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -14,7 +14,7 @@ public class Admin {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotNull(message = "Username cannot be null")
     private String username;
 
     @NotNull
